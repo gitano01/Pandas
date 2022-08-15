05f6537899b229f47c38eaeb48e7d7c618b9a04c
lobus <vramirez.info@gmail.com>
2022-08-15 18:26:09 -0500

    ajuste de la duracion del token_fix

--- jwtServerOAuth/src/main/java/com/efectivale/jwtServer/config/SwaggerConfig.java
+++ jwtServerOAuth/src/main/java/com/efectivale/jwtServer/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.efectivale.jwtServer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.efectivale.jwtServer.utils.ConstantesJwt;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,6 +19,7 @@ public class SwaggerConfig {
 	
 		@Bean
 		public Docket api() {
+	
 			Docket docket =  new Docket(DocumentationType.SWAGGER_2);
 			docket.select()
 					.apis(RequestHandlerSelectors.basePackage("com.efectivale.jwtServer.controller"))
--- jwtServerOAuth/src/main/java/com/efectivale/jwtServer/validator/AuthValidator.java
+++ jwtServerOAuth/src/main/java/com/efectivale/jwtServer/validator/AuthValidator.java
@@ -28,9 +28,4 @@ public class AuthValidator {
 		return apiResponse;
 		
 	}
-	
-	
-	
-
-	
 }
